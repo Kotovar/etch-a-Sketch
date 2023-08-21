dc49a17529278b2dec8b914caf6b2886e904ab87
Kotovar <77914431+Kotovar@users.noreply.github.com>
2023-08-22 00:28:53 +0700

    add choice color

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let container = document.getElementById("container");
+let container = document.getElementById("container"); //основное поле
+let color = document.getElementById("color");
+let colorActive = "black";
+let button = document.getElementById("reset");
 container.style.display = "grid";
+
+//функция для отслеживания, какой цвет выбран из палитры
+color.addEventListener("input", function (e) {
+  colorActive = e.target.value;
+});
 
 //функция для создания сетки по аргументу
 function create(number) {
@@ -16,34 +24,63 @@ function create(number) {
 
     container.appendChild(cell);
     cell.classList.add("cell");
-    // listener();
   }
 }
 
 create(16); //стартовая сетка
 
-//создание эффекта зависания мыши
+//рисование по сетке выбранным цветом
 
 container.addEventListener("mouseover", function (e) {
   if (e.target.classList.contains("cell")) {
-    e.target.style.backgroundColor = "red";
+    // let computedStyle = window.getComputedStyle(e.target);
+    // let currentColor = computedStyle.backgroundColor;
+
+    e.target.style.backgroundColor = colorActive;
+
+    // switch (currentColor) {
+    //   case "rgba(0, 0, 0, 0)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.1)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.2)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.3)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.4)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.5)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.6)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.7)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.8)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+    //     break;
+    //   case "rgba(0, 0, 0, 0.9)":
+    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 1.0)";
+    //     break;
+    // }
   }
 });
 
-// container.addEventListener("mouseout", function (e) {
-//   if (e.target.classList.contains("cell")) {
-//     e.target.style.backgroundColor = "white";
-//   }
-// });
-
 // настройка смены размера поля
-let button = document.getElementById("reset");
+
 button.addEventListener("click", function () {
   let choice = prompt("Какой размер?");
   if (choice >= 2 && choice <= 100) {
     container.replaceChildren();
     create(choice);
   } else {
-    alert("Так нельзя");
+    alert("некорректный ввод");
   }
 });
